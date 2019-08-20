5b3369b57de4d5bdfe6e4d5fb93ba7f70cda7684
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-20 12:52:59 +0530

    Update login.php

--- login.php
+++ login.php
@@ -39,12 +39,13 @@
          </div>
       </section>
 	 <?php
-	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
 		if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
     {
         abc();
     }
 	   function abc(){
+		   	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
+
                      $sql = "select firstname from contact.contact";
 			    
                             $resultset = pg_query($dbconn, $sql);
