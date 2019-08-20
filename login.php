9e4a78b1af5bdad611360df00f81e7a78ba07e11
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-20 13:06:47 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,11 +11,18 @@
                <div class="col-md-6 col-sm-12 col-lg-6 login-form">
                   <div id="login-container">
                      <form action="" method="post" role="form" class="contactForm">
-                        <div class="form-group">
+                        <?php
+				$my_var = <<<EOD
+						
+					         
+			     
+			     
+			     <div class="form-group">
                            <input type="email" class="form-control login-form-input" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                            <div class="validation"></div>
                         </div>
-						
+			EOD;
+					?>			
                         <div class="form-group">
                            <input type="password" class="form-control login-form-input" name="password" id="password" placeholder="Password" data-rule="minlen:4" data-msg="Please enter correct password" />
                            <div class="validation"></div>
@@ -46,7 +53,7 @@
 	   function abc(){
 		   	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
 
-                     $sql = "select firstname from contact.contact where email=' ". $email. " '";
+                     $sql = "select firstname from contact.contact where email=' ". $my_var. " '";
 			    
                             $resultset = pg_query($dbconn, $sql);
                             while($row = pg_fetch_array($resultset)) {
@@ -57,6 +64,7 @@
                                     </tr>'; 
                             }
 				echo 'hello';
+		               echo $my_var;
                             pg_close($dbconn); 
 	   }
 ?>
