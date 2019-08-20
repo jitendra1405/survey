278358f86e2b7bdedcfbc5e96741d07167ede2bd
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-20 13:19:13 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,8 +12,10 @@
                   <div id="login-container">
                      <form action="" method="post" role="form" class="contactForm">
                            <div class="form-group">
+				   <form action="theSamePage.php" method="post">
                            <input type="email" class="form-control login-form-input" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                           <div class="validation"></div>
+				   </form>
+					   <div class="validation"></div>
                         </div>
 					
                         <div class="form-group">
@@ -42,11 +44,12 @@
 		if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
     {
         abc();
+        $formvalue = $_POST['email'];
     }
 	   function abc(){
 		   	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
-			$formvalue = $_POST['email'];
-                     $sql = "select firstname from contact.contact where email='';
+			
+                     $sql = "select firstname from contact.contact;
 			    
                             $resultset = pg_query($dbconn, $sql);
                             while($row = pg_fetch_array($resultset)) {
