acf870c502c6128a021699e1c6a5e99a40e338b6
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-20 12:33:33 +0530

    Update login.php

--- login.php
+++ login.php
@@ -45,8 +45,7 @@ $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 d
                             $resultset = pg_query($dbconn, $sql);
                             while($row = pg_fetch_array($resultset)) {
                                 $size = count($row);
-				    if($size > 0)
-					    header('location: survey.php');
+				   echo $size;
                             }
 
                             pg_close($dbconn);   
