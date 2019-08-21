b5b20f3dc26fcfe6bd4d6559350cee1a406632ac
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-21 18:06:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
    
       <section id="login-section">
          <div class="container">
-            <div class="row">
+            <!--<div class="row">
 				<div class="col-md-6" ><a href="login.php">Login</a></div>
 				<div class="col-md-6"><a href="user-registration.php">Sign up</a></div>
-			</div>
+			</div>-->
 		</div>
 	</section>
 	  
