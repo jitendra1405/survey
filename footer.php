69cd6a205670d1c55730bf240f90f7e2170f21e7
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-21 17:01:02 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -18,13 +18,7 @@
                     <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
                   </div>
 
-                  <div class="footer-newsletter">
-                    <h4>Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem.</p>
-                    <form action="" method="post">
-                      <input type="email" name="email"><input type="submit"  value="Subscribe">
-                    </form>
-                  </div>
+                  
 
                 </div>
 
@@ -40,7 +34,20 @@
                     </ul>
                   </div>
 
-                  <div class="footer-links">
+                  
+
+                  
+
+                </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+		  <div class="row">
+		  <div class="col-md-6">
+			<div class="footer-links">
                     <h4>Contact Us</h4>
                     <p>
                       A108 Adam Street <br>
@@ -50,54 +57,21 @@
                       <strong>Email:</strong> info@example.com<br>
                     </p>
                   </div>
+			</div>
 
-                  <div class="social-links">
-                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+				<div class="col-md-6">
+			<div class="footer-newsletter">
+                    <h4>Our Newsletter</h4>
+                    
+                    <form action="" method="post">
+                      <input type="email" name="email"><input type="submit"  value="Subscribe">
+                    </form>
                   </div>
-
-                </div>
-
             </div>
 
           </div>
 
-          <div class="col-lg-6">
-
-            <div class="form">
-              
-              <h4>Send us a message</h4>
-              <p>Eos ipsa est voluptates. Nostrum nam libero ipsa vero. Debitis quasi sit eaque numquam similique commodi harum aut temporibus.</p>
-              <form action="" method="post" role="form" class="contactForm">
-                <div class="form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validation"></div>
-                </div>
-
-                <div id="sendmessage">Your message has been sent. Thank you!</div>
-                <div id="errormessage"></div>
-
-                <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
-              </form>
-            </div>
-
           </div>
-
-          
 
         </div>
 
