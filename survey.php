5f8633c8054f0d38babd8ca72852a0cef83f6e35
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-21 17:36:28 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -1,215 +1,55 @@
-<!DOCTYPE html>
 <html>
-   <title>Survey</title>
-   <!--/////////////////Head Include/////////-->
-  
-   <!--/////////////////////////////////////-->
-   <body>
-  
-     <!-- <section class="survey">
-         <div class="container" >
-            <div class="row">
-				<div class="col-md-12">
-					<form>
-						<div class="form-group survey-question-wrapper has-separator">
-							<label for="" class="survey-question">Highest Level of Education Completed:</label>
-							<div class="answer-container">
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="Good" id="Good" value="Good">
-							  <label class="survey-answer" for="">
-								Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="VeryGood" id="VeryGood" value="Very Good ">
-							  <label class="survey-answer" for="">
-								Very Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="excellent" id="excellent" value="excellent">
-							  <label class="survey-answer" for="">
-								Excellent
-							  </label>
-							</div>
-							</div>
-						</div>
-						
-						<div class="form-group survey-question-wrapper has-separator">
-							<label for="" class="survey-question">Highest Level of Education Completed:</label>
-							<div class="answer-container">
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="Good" id="Good" value="Good">
-							  <label class="survey-answer" for="">
-								Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="VeryGood" id="VeryGood" value="Very Good ">
-							  <label class="survey-answer" for="">
-								Very Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="excellent" id="excellent" value="excellent">
-							  <label class="survey-answer" for="">
-								Excellent
-							  </label>
-							</div>
-							</div>
-						</div>
-						
-						<div class="form-group survey-question-wrapper has-separator">
-							<label for="" class="survey-question">Highest Level of Education Completed:</label>
-							<div class="answer-container">
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="Good" id="Good" value="Good">
-							  <label class="survey-answer" for="">
-								Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="VeryGood" id="VeryGood" value="Very Good ">
-							  <label class="survey-answer" for="">
-								Very Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="excellent" id="excellent" value="excellent">
-							  <label class="survey-answer" for="">
-								Excellent
-							  </label>
-							</div>
-							</div>
-						</div>
-						
-						<div class="form-group survey-question-wrapper has-separator">
-							<label for="" class="survey-question">Highest Level of Education Completed:</label>
-							<div class="answer-container">
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="Good" id="Good" value="Good">
-							  <label class="survey-answer" for="">
-								Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="VeryGood" id="VeryGood" value="Very Good ">
-							  <label class="survey-answer" for="">
-								Very Good
-							  </label>
-							</div>
-							<div class="form-check answer-options">
-							  <input class="form-check-input" type="radio" name="excellent" id="excellent" value="excellent">
-							  <label class="survey-answer" for="">
-								Excellent
-							  </label>
-							</div>
-							</div>
-						</div>
-						
-					</form>
-				</div>
-            </div>
-         </div>
-      </section>-->
-	  
-	  <!---Footer--->
-	  
-
-	    <?php
-       	    
-	    	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
-			$abc = "";
+<body>
+<form method="post">
+    <?php
+	$dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
+			
                      $sql = "select SFID,Question_1__c from contact.question__c";
 	           
                             $resultset = pg_query($dbconn, $sql);
 	   		   
 		            $num_rows = pg_num_rows($resultset);
-	   		    for($i=0;$i<$num_rows;$i++){
-				    echo $i;
+	   		  
                             while($row = pg_fetch_array($resultset)) {
-				    
-				    echo '<tr>
-                                        <td><div class="form-group survey-question-wrapper has-separator">
-							<label for="" class="survey-question">'.$row[1].'<br/></label></td>
-                                        
-                                    </tr>'; 
-				    
-				$sql1 = "select Option_1__c,Option_2__c,Option_3__c from contact.Option__c where question__c ='$row[0]'";
+    for($i=1;$i<=$num_rows;$i++)
+    {
+        ?>
+        <div class="well well-sm well-primary">
+            <input type="hidden" name="ques"/><?php echo $row[1]; ?>
+        </div>
+	<?php
+	    $sql1 = "select Option_1__c,Option_2__c,Option_3__c from contact.Option__c where question__c ='$row[0]'";
 					    $resultset1 = pg_query($dbconn, $sql1);    
                                
 				$num_rows = pg_num_rows($resultset);
 	                   while($row1 = pg_fetch_array($resultset1)) {
-                                 
-				   echo '<form method="POST">
-				        <br/><tr>
-                                        <td><input  type="radio" name="status[$i]" value="' . $row1[0] . '">'.$row1[0].'<br/></td>
-                                        <td><input  type="radio" name="status[$i]" value="' . $row1[1] . '">'.$row1[1].'<br/></td>
-					<td><input  type="radio" name="status[$i]" value="' . $row1[2] . '">'.$row1[2].'<br/></td>
-					
-                                    <br/></tr>
-				   </form>
-				    ';
-				   		   
-				   
-					
-				    
-                            }
-	             		$radioVal = $_POST["status[$i]"];
-				   $abc = $radioVal;
-			  }
-             
- }
-	   
-		   
-	             
-	   
-	    if(isset($_POST['Result'])){
-					 foreach($_POST['status'] as $key => $value){ 
-    						echo "Question No. $key, Value = $value<br>"; 
-						} 
-			     		
-			     }	               
-	   
-	  			
-	   
-		            
-                            pg_close($dbconn); 
-	   
-?>
-<form method="POST">	  	 
-<input type="submit" value="Result" name="Result">  </form>	   
-	
-<button id="upload_widget" class="cloudinary-button">Upload files</button>
-  
-
-<script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>  
-
-<script type="text/javascript"> 
-  
-  var url;
-var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'ddumb92d7', 
-  uploadPreset: 'vqqpvdfw'}, (error, result) => { 
-    if (!error && result && result.event === "success") { 
-      console.log('Done! Here is the image info: ', result.info); 
-    url  = console.log('urllllll: ',result.info.eager[0].url);
-    
-      
+				   ?>
+        <div class="well well-sm">
+            <div class="radio">
+                <label>
+                <input type="radio" name="optradio[<?php echo $i; ?>]" value="' . $row1[0] . '"><?php echo $row1[0]; ?></label>
+            </div>
+            <div class="radio">
+                <label>
+                <input type="radio" name="optradio[<?php echo $i; ?>]" value="' . $row1[1] . '"><?php echo $row1[1]; ?></label>
+            </div>
+            <div class="radio">
+                <label>
+                <input type="radio" name="optradio[<?php echo $i; ?>]" value="' . $row1[2] . '"><?php echo $row1[2]; ?></label>
+            </div>
+        </div>
+        <?php
     }
-  }
-)
-document.getElementById("upload_widget").addEventListener("click", function(){
-    myWidget.open();
-  }, false);
-  
-  
-</script>   
-	   
-	   
-	   
+    }
+    ?>
+    <button type="submit" class="btn btn-success" name="submit">Finish</button>
 	
-	   
-	   
-   </body>
+ <?php 
+ foreach($_POST['optradio'] as $option_num => $option_val){
+    echo $option_num." ".$option_val."<br>";}
+	 pg_close($dbconn); 			    
+?>
+
+</form>
+</body>
 </html>
