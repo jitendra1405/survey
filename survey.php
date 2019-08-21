c7aac2899d44b03a11597ef45980e38dbe283709
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-21 15:10:48 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -144,7 +144,7 @@
                                         <td><input  type="radio" name="status" value="' . $row1[0] . '">'.$row1[0].'<br/></td>
                                         <td><input  type="radio" name="status" value="' . $row1[1] . '">'.$row1[1].'<br/></td>
 					<td><input  type="radio" name="status" value="' . $row1[2] . '">'.$row1[2].'<br/></td>
-					<input type="submit" value="Result" name="Result">
+					
                                     <br/></tr>
 				    </form>
 				    ';
@@ -156,6 +156,7 @@
                             }
 	             
 			  }
+                           <input type="submit" value="Result" name="Result">
 	                  if(isset($_POST['Result'])){
 			     		echo $abc;
 			     }	 
