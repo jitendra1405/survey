164d7ed80064fb4a904cbb08c051656b97991dab
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-21 16:02:08 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -114,7 +114,8 @@
       </section>-->
 	  
 	  <!---Footer--->
-	 
+	 <form method="POST">	   
+<input type="submit" value="Result" name="Result">  </form>
 	    <?php
        	    
 	    	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
@@ -168,8 +169,7 @@
 ?>
 	 
 	   
-<form method="POST">	   
-<input type="submit" value="Result" name="Result">  </form>	
+	
 <button id="upload_widget" class="cloudinary-button">Upload files</button>
   
 
