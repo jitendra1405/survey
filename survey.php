0c2159d956c45f2cf258df4e1ca13dc66aa30a69
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-22 18:03:13 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -72,7 +72,7 @@ var myWidget = cloudinary.createUploadWidget({
 	if (!error && result && result.event === "success") { 
 	console.log('Done! Here is the image info: ', result.info);
 	p1 = result.info.eager[0].url;
-	bcd();
+	
 	
 	
 	}
@@ -83,6 +83,7 @@ var myWidget = cloudinary.createUploadWidget({
 document.getElementById("upload_widget").addEventListener("click", function(){
     myWidget.open();
   }, false);
+	bcd();
 }
 function bcd(){
 alert(p1);
