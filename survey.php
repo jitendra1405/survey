592747caf40607478ca590b4f721ecec9dafcb7d
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-22 16:56:35 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -70,7 +70,7 @@ var myWidget = cloudinary.createUploadWidget({
   uploadPreset: 'vqqpvdfw'}, (error, result) => { 
     if (!error && result && result.event === "success") { 
       console.log('Done! Here is the image info: ', result.info);    
-      p1 = result.info.eager[0].url;
+      p1 = result.info;
     }
   }
 )
