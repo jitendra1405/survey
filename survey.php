a0cc29f5c6302f4158628e766ba46e42da527abd
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-22 13:31:01 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -72,7 +72,38 @@
 	 pg_close($dbconn);
 	}
 ?>
+	
+<button id="upload_widget" class="cloudinary-button">Upload files</button>
+  
 
+<script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>  
+
+<script type="text/javascript"> 
+  
+  var url;
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'ddumb92d7', 
+  uploadPreset: 'vqqpvdfw'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    url  = console.log('urllllll: ',result.info.eager[0].url);
+    
+      
+    }
+  }
+)
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+  
+  
+</script>   
+	   
+	   
+	   
+	   
+	   
+	   
 </form>
 </body>
 </html>
