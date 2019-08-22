267d7dadb775e3e06778da25f15ead1fb3d2ac17
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-22 13:06:09 +0530

    Update login.php

--- login.php
+++ login.php
@@ -49,6 +49,8 @@
 	   function abc(){
 		   	   $dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
 			$formvalue = $_POST['email'];
+		        $_SESSION['var'] = $formvalue;
+
 		        $formvalue1 = $_POST['password'];
                      $sql = "select firstname from contact.contact where email='$formvalue' AND Lastname='$formvalue1'";	
                             $resultset = pg_query($dbconn, $sql);
