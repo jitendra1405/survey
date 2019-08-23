9bb65a2e7f1226ea15da5a275d6ee2b30dc16607
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-08-23 12:14:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,274 @@
-<?php include 'header.php';?>
+<!DOCTYPE html>
 <html>
    <!--/////////////////Head Include/////////-->
-   <style>
-	#survey-intro {
-    width: 100%;
-    height: 40vh;
-    position: relative;
-    background: #f5f8fd url(../img/intro-bg.jpg) center top no-repeat;
-    background-size: cover;
-}
-   </style>
+   <?php include 'header.php';?>
    <!--/////////////////////////////////////-->
+   <style>
+      .page-title-block{
+      margin: 0px 0px 60px 0px;
+      text-align:center;
+      padding: 10px 0px 0px 0px;
+      background-image: url(//cdn.jevelin.shufflehound.com/wp-content/uploads/2016/03/Home_5_element_1.jpg);
+      background-repeat: no-repeat;
+      background-size: auto;
+      background-position: center center;
+      }
+      .page-title{
+      font-size: 24px;
+      font-weight: 400!important;
+      color: #8d8d8d;
+      text-align: center;
+      margin-bottom: 0px;
+      margin-top: 12px;
+      }
+      .heading-content {
+      font-size: 40px;
+      font-family: "Montserrat";
+      color: #505050;
+      font-weight: 700;
+      text-align:center;
+      }
+   </style>
    <body>
-   <section id="survey-intro" class="clearfix">
-    <div class="container d-flex h-100">
-      
-
-    </div>
-  </section>
-   
-   
-      <section id="login-section">
+      <!--==========================
+         Intro Section
+         ============================-->
+      <section id="intro" class="clearfix">
+         <div class="container d-flex h-100">
+            <div class="row justify-content-center align-self-center">
+               <div class="col-md-12 intro-info order-md-first order-last">
+                  <h2 style="color:#0ae2e7">Customer Survey<br/> <span class="txt-rotate"  data-period="2000" data-rotate='[ "For Your Business!", "Customer Satisfaction!", "Happy Customer!" ]'></span></h2>
+                  <!--<div>
+                     <a href="#" class="btn-get-started scrollto">Get Start Free</a>
+                     </div>-->
+               </div>
+               <!--<div class="col-md-6 intro-img order-md-last order-first">
+                  <img src="img/intro-img.svg" alt="" class="img-fluid">
+                  </div>-->
+            </div>
+         </div>
+      </section>
+      <!-- #intro -->
+      <!--==========================
+         Features Section
+         ============================-->
+      <section id="features">
          <div class="container">
-            <!--<div class="row">
-				<div class="col-md-6" ><a href="login.php">Login</a></div>
-				<div class="col-md-6"><a href="user-registration.php">Sign up</a></div>
-			</div>-->
-		</div>
-	</section>
-	  
-	  
+            <div class="row">
+               <div class="col-md-12 wow fadeInUp page-title-block">
+                  <h2 class="page-title">What is Lorem Ipsum?</h2>
+                  <h3 class="heading-content">Where does it come from?</h3>
+               </div>
+            </div>
+            <div class="row feature-item">
+               <div class="col-lg-4 wow fadeInUp business-s">
+               </div>
+               <div class="col-lg-8 wow fadeInUp pt-5 pt-lg-0 sh-box">
+                  <div style="    padding: 8%;">
+                     <h4>Why do we use it?</h4>
+                     <p class="text-justify">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                     </p>
+                  </div>
+               </div>
+            </div>
+            <div class="row feature-item">
+               <div class="col-lg-8 wow fadeInUp pt-5 pt-lg-0 sh-box">
+                  <div style="    padding: 8%;">
+                     <h4>Why do we use it?</h4>
+                     <p class="text-justify">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                     </p>
+                  </div>
+               </div>
+               <div class="col-lg-4 wow fadeInUp customer-s">
+               </div>
+            </div>
+            <div class="row feature-item">
+               <div class="col-lg-4 wow fadeInUp customer-r">
+               </div>
+               <div class="col-lg-8 wow fadeInUp pt-5 pt-lg-0 sh-box">
+                  <div style="    padding: 8%;">
+                     <h4>Why do we use it?</h4>
+                     <p class="text-justify">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- #about -->
+      <!--==========================
+         Clients Section
+         ============================-->
+      <section id="testimonials">
+         <div class="container">
+            <header class="section-header">
+               <h3>Testimonials</h3>
+            </header>
+            <div class="row justify-content-center">
+               <div class="col-lg-8">
+                  <div class="owl-carousel testimonials-carousel wow fadeInUp">
+                     <div class="testimonial-item">
+                        <img src="img/testimonial-1.jpg" class="testimonial-img" alt="">
+                        <h3>Saul Goodman</h3>
+                        <h4>Ceo &amp; Founder</h4>
+                        <p>
+                           Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                        </p>
+                     </div>
+                     <div class="testimonial-item">
+                        <img src="img/testimonial-2.jpg" class="testimonial-img" alt="">
+                        <h3>Sara Wilsson</h3>
+                        <h4>Designer</h4>
+                        <p>
+                           Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                        </p>
+                     </div>
+                     <div class="testimonial-item">
+                        <img src="img/testimonial-3.jpg" class="testimonial-img" alt="">
+                        <h3>Jena Karlis</h3>
+                        <h4>Store Owner</h4>
+                        <p>
+                           Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                        </p>
+                     </div>
+                     <div class="testimonial-item">
+                        <img src="img/testimonial-4.jpg" class="testimonial-img" alt="">
+                        <h3>Matt Brandon</h3>
+                        <h4>Freelancer</h4>
+                        <p>
+                           Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- #testimonials -->
+      <!--==========================
+         Frequently Asked Questions Section
+         ============================-->
+      <section id="faq">
+         <div class="container">
+            <header class="section-header">
+               <h3>Frequently Asked Questions</h3>
+               <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+            </header>
+            <ul id="faq-list" class="wow fadeInUp">
+               <li>
+                  <a data-toggle="collapse" class="collapsed" href="#faq1">Non consectetur a erat nam at lectus urna duis? <i class="ion-android-remove"></i></a>
+                  <div id="faq1" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                     </p>
+                  </div>
+               </li>
+               <li>
+                  <a data-toggle="collapse" href="#faq2" class="collapsed">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i class="ion-android-remove"></i></a>
+                  <div id="faq2" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                     </p>
+                  </div>
+               </li>
+               <li>
+                  <a data-toggle="collapse" href="#faq3" class="collapsed">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="ion-android-remove"></i></a>
+                  <div id="faq3" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                     </p>
+                  </div>
+               </li>
+               <li>
+                  <a data-toggle="collapse" href="#faq4" class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="ion-android-remove"></i></a>
+                  <div id="faq4" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                     </p>
+                  </div>
+               </li>
+               <li>
+                  <a data-toggle="collapse" href="#faq5" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="ion-android-remove"></i></a>
+                  <div id="faq5" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+                     </p>
+                  </div>
+               </li>
+               <li>
+                  <a data-toggle="collapse" href="#faq6" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="ion-android-remove"></i></a>
+                  <div id="faq6" class="collapse" data-parent="#faq-list">
+                     <p>
+                        Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
+                     </p>
+                  </div>
+               </li>
+            </ul>
+         </div>
+      </section>
+      <!-- #faq -->
+      <script>	
+         var TxtRotate = function(el, toRotate, period) {
+          this.toRotate = toRotate;
+          this.el = el;
+          this.loopNum = 0;
+          this.period = parseInt(period, 10) || 2000;
+          this.txt = '';
+          this.tick();
+          this.isDeleting = false;
+         };
+         
+         TxtRotate.prototype.tick = function() {
+          var i = this.loopNum % this.toRotate.length;
+          var fullTxt = this.toRotate[i];
+         
+          if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+          } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+          }
+         
+          this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+         
+          var that = this;
+          var delta = 300 - Math.random() * 100;
+         
+          if (this.isDeleting) { delta /= 2; }
+         
+          if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+          } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+          }
+         
+          setTimeout(function() {
+            that.tick();
+          }, delta);
+         };
+         
+         window.onload = function() {
+          var elements = document.getElementsByClassName('txt-rotate');
+          for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-rotate');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
+          }
+          // INJECT CSS
+          var css = document.createElement("style");
+          css.type = "text/css";
+          css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #0ae2e7 }";
+          document.body.appendChild(css);
+         };
+      </script>
       <!--///////////////////////Footer Include//////////////////////////////-->
-      
+      <?php include 'footer.php';?>
    </body>
 </html>
-<?php include 'footer.php';?>
